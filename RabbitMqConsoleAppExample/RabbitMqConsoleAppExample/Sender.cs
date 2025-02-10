055bb995b01488ae390346c9172b102678adb952
Maciej Goryczko <goryczko99@gmail.com>
2025-02-10 19:12:20 +0100

    create channel and publish message

---
+++ RabbitMqConsoleAppExample/RabbitMqConsoleAppExample/Sender.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+var routeKey = "BasicTest";
+
+using (var connection = await factory.CreateConnectionAsync())
+using (var channel = await connection.CreateChannelAsync())
+{
+    await channel.QueueDeclareAsync(
+        routeKey,
+        false,
+        false,
+        false,
+        null
+        );
+
+    var message = "Sample string message";
+
+    var body = Encoding.UTF8.GetBytes( message );
+
+    await channel.BasicPublishAsync("", routeKey, body);
+    Console.WriteLine($"Sent message to {routeKey}. Content: {message}.");
+    Console.ReadLine();
+}
